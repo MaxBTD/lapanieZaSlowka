97a0f739b7a6fe9c81b73e08cbd800f8b2a71bcf
5m <konradek982@gmail.com>
2024-12-13 09:54:19 +0100

    Dodaj pliki projektów.

---
+++ lapanieZaSlowka/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace lapanieZaSlowka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sent1 = Console.ReadLine().Split(' ');
+            string[] sent2 = Console.ReadLine().Split(' ');
+            string? rozneSlowa = "";
+
+            foreach(string s in sent1)
+                if (!sent2.Contains(s))
+                    rozneSlowa += $" {s}";
+
+            foreach(string s in sent2)
+                if(!sent1.Contains(s))
+                    rozneSlowa += $" {s}";
+
+            Console.WriteLine(rozneSlowa.Trim());
+        }
+    }
+}
